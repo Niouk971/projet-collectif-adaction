1413ac7bfa840d5fa90b4c80930c3bb87c994fdc
Félix Prudhomme <felixprudhomme@yahoo.com>
2025-09-30 15:13:50 +0200

    initiation du front

--- backend/server.js
+++ backend/server.js
@@ -62,4 +62,15 @@ app.get("/menu/:id", (req, res) => {
     res.json(plat);
 });
 
+app.post("/orders", (req, res) => {
+    const { id, plate, clientName } = req.body;
+    if (!id || !plate || !clientName) {
+        return res.status(400).json({ error: "Champs manquants ou invalides" });
+    };
+
+    console.log(`[COMMANDE REÇUE] id=${id} | plat="${plate}" | client="${clientName}"`);
+    // remplacer par insert into (sql)
+    return res.status(201).json({ ok: true, message: `Commande reçue ${plate} pour ${clientName}` });
+});
+
 app.listen(3000, () => { console.log("Serveur lancé sur http://localhost:3000"); });
